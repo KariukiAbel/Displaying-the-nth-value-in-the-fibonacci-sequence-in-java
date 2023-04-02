a7cc0d79054e069e3092694d8e92a9edc0b4cb19
KariukiAbel <abelmuriithi@gmail.com>
2023-04-02 18:52:14 +0300

    Done some refactoring to correct spelling mistakes

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan  = new Scanner(System.in);
-        System.out.println("Input the a number and I will tell you it's value in the fibonacci sequence");
+        System.out.println("Input a number and I will tell you it's value in the fibonacci sequence");
         int n = scan.nextInt();
         int v1 = 1;
         int result = 0, v2 = v1;
